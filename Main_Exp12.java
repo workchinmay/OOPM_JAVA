082f3e6ae46435028bdc27d25ab8110708de7129
workchinmay <145839915+workchinmay@users.noreply.github.com>
2023-11-03 19:15:01 +0530

    Create Main_Exp12.java

---
+++ Main_Exp12.java
@@ -0,0 +1,22 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
+
+        thread1.start();
+        thread2.start();
+    }
+}
